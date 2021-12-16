a52bbf77b3f6d6fd955c00c08c1b98ae7b7477c0
Aug3nst3rn28 <boonboon1128@gmail.com>
2021-12-16 20:17:49 +0800

    Added card for room, a div and footer

--- view/home/index.php
+++ view/home/index.php
@@ -5,12 +5,16 @@ include_once('../../config.php');
 <html lang="en">
 
 <head>
+    <!-- Import meta -->
     <?php include_once(DIR_LAYOUT . 'head.php'); ?>
     <title>Login | Genibenix</title>
 </head>
 
 <body>
+    <!-- Import Header Nav Bar -->
     <?php include_once(DIR_LAYOUT . 'user_navbar.php'); ?>
+
+    <!-- Carousel -->
     <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -28,24 +32,50 @@ include_once('../../config.php');
                 <img src="../../image/carousel_slide3.png" class="d-block w-100" alt="Hall View">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
 
+    <div id="roomDiv">
+        <h1 class="text-center my-5">Our Room and Rates</h1>
+    </div>
+    <!-- Bootstrap Card to show the rooms and the prices -->
     <div class="container mt-4">
-        <div class="row">
+        <div class="row mb-3">
             <div class="col-sm-3">
                 <div class="card text-center" style="width:auto">
                     <img class="card-img-top" src="../../image/singleroom.jpeg" alt="Card image" style="width:100%">
                     <div class="card-body">
                         <h4 class="card-title">Single Room</h4>
-                        <p class="card-text">Rm 60</p>
+                        <p class="card-text">RM 60 per day</p>
+                        <a href="#" class="btn btn-primary">Book Now!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card text-center" style="width:auto">
+                    <img class="card-img-top" src="../../image/doubleroom.jpeg" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">Double Room</h4>
+                        <p class="card-text">RM 90 per day</p>
+                        <a href="#" class="btn btn-primary">Book Now!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card text-center" style="width:auto">
+                    <img class="card-img-top" src="../../image/luxuryroom.jpeg" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">Luxury Room</h4>
+                        <p class="card-text">RM 130 per day</p>
+                        <a href="#" class="btn btn-primary">Book Now!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card text-center" style="width:auto">
+                    <img class="card-img-top" src="../../image/deluxeroom.jpeg" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">Deluxe Room</h4>
+                        <p class="card-text">RM 180 per day</p>
                         <a href="#" class="btn btn-primary">Book Now!</a>
                     </div>
                 </div>
@@ -53,6 +83,19 @@ include_once('../../config.php');
         </div>
     </div>
 
+    <div class="bg-light text-center p-5 mt-5">
+        <h1>Service is our mission</h1>
+        <blockquote class="blockquote text-center p-5">
+            <p class="px-5">“There is a spiritual aspect to our lives — when we give, we receive —
+                when a business does something good for somebody, that somebody feels
+                good about them!”</p>
+            <footer class="blockquote-footer">Ben Cohen, <cite title="Source Title">Co-Founder Ben & Jerry’s</cite></footer>
+        </blockquote>
+
+    </div>
+  
+    <!-- Import Footer -->
+    <?php include_once(DIR_LAYOUT . 'footer.php'); ?>
     <?php include_once(DIR_SYSTEM . 'globaljs.php'); ?>
 </body>
 
