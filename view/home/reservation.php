14009b560d7cf24d4ad6273dd21e511d9b2f4d91
Aug3nst3rn28 <boonboon1128@gmail.com>
2021-12-16 20:46:13 +0800

    created reservation page

---
+++ view/home/reservation.php
@@ -0,0 +1,11 @@
+<?php
+include_once('../../config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Import meta -->
+    <?php include_once(DIR_LAYOUT . 'head.php'); ?>
+    <title>Reservation | Genibenix</title>
+</head>
