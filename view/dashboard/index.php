04535a87af38f8d2841e32131cbddfe6355083fc
Aug3nst3rn28 <boonboon1128@gmail.com>
2021-12-16 13:33:28 +0800

    FIx dashboard/index.php footer

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -16,7 +16,7 @@ include_once('../../config.php');
             ?>
         </div>
     </div>
-    <?php include_once(DIR_LAYOUT . '/footer.php'); ?>
+    <?php include_once(DIR_LAYOUT . 'footer.php'); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
