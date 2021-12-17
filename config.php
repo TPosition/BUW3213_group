df8cc5d22ecdbf8eacc1d79d6fa29a87e8382bb7
TPosition <zetanalpha@gmail.com>
2021-12-17 17:39:52 +0800

    Add active highlight

--- config.php
+++ config.php
@@ -4,3 +4,7 @@ define('DIR_SYSTEM', DIR_BASE . 'BUW3213_group/');
 define('DIR_VIEW', DIR_SYSTEM . 'view/');
 define('DIR_LAYOUT', DIR_SYSTEM . 'layout/');
 define('DIR_IMAGE', DIR_SYSTEM . 'image/');
+
+define('URI_BASE', '/');
+define('URI_SYSTEM', URI_BASE . 'BUW3213_group/');
+define('URI_VIEW', URI_SYSTEM . 'view/');
