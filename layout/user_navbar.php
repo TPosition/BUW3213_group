5a4890bf5c28463eaa84138ea3569677f1ec001b
Aug3nst3rn28 <boonboon1128@gmail.com>
2021-12-19 15:22:00 +0800

    booking history page Ui + header update

--- layout/user_navbar.php
+++ layout/user_navbar.php
@@ -6,9 +6,8 @@
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <li><a href="index.php" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#roomDiv" class="nav-link px-2 link-dark">Rooms</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+            <li><a href="index.php#roomDiv" class="nav-link px-2 link-dark">Rooms</a></li>
+            <li><a href="bookinghistory.php" class="nav-link px-2 link-dark">My Booking</a></li>
         </ul>
 
         <div class="col-md-3 text-end">
