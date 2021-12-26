6708101b2ec7bf2c43aa7fb8a17b3c8e88725d66
shaoming123 <greatfoodprefer@gmail.com>
2021-12-26 22:36:09 +0800

    date problem

--- action/reset_password.php
+++ action/reset_password.php
@@ -6,38 +6,40 @@ include_once('../../config.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
-    $new_password = ($_POST["new_password"]);
+    if (isset($_POST["new_password"])) {
+        $new_password = ($_POST["new_password"]);
 
-    // Prepare an update statement
-    $sql = "UPDATE user SET password = ? WHERE id = ?";
+        // Prepare an update statement
+        $sql = "UPDATE user SET password = ? WHERE id = ?";
 
-    if ($stmt = mysqli_prepare($link, $sql)) {
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
 
-        // Set parameters
-        $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-        $param_id = $_SESSION["id"];
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
 
 
-        if (mysqli_stmt_execute($stmt)) {
-            // Password updated successfully. Destroy the session, and redirect to login page
-            session_destroy();
-            echo "<script>  
+            if (mysqli_stmt_execute($stmt)) {
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                echo "<script>  
                 alert('Update Successful');  
                 window.location='../login/index.php'
                  </script>";
-            exit();
-        } else {
-            echo "Oops! Something went wrong. Please try again later.";
+                exit();
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
 
-        // Close statement
-        mysqli_stmt_close($stmt);
+
+
+        // Close connection
+        mysqli_close($link);
     }
-
-
-
-    // Close connection
-    mysqli_close($link);
 }
